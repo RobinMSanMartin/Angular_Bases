fc8315c3ba6b98e7aceeb8270da6c17ec4da026c
Robin San Martin <robin.sanmartin.venegas@ciisa.cl>
2025-11-20 21:53:49 -0300

    added the basics of inputs and signals

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -12,12 +12,37 @@ interface Character{
   // imports:[NgClass]
 })
 export class DragonballPageComponent {
+
+  name = signal('Gohan');
+  power = signal(100);
+
  characters = signal<Character[]>([
   {id:1, name:'Goku', power:9001},
   {id:2, name:'Vegeta', power:8000},
   {id:3, name:'Piccolo', power:3000},
   {id:4, name:'Yamcha', power:500},
  ]);
+
+ addCharacter(){
+  if(!this.name() || !this.power() || this.power() <= 0){
+    return;
+  }
+
+  const newCharacter:Character ={
+    id: this.characters.length+1,
+    name: this.name(),
+    power: this.power()
+  }
+
+  // this.characters().push(newCharacter);
+  this.characters.update((list) => [... list, newCharacter]);
+  this.resetFields();
+ }
+
+ resetFields(){
+  this.name.set('');
+  this.power.set(0);
+ }
 
 //  powerClasses = computed(() => {
 //   return {
